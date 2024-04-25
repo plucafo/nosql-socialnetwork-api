144dd01bba0fca031ce9fb40ecbc451eef975fab
plucafo <plucaf87@gmail.com>
2024-04-25 09:30:49 -0700

    added routes to find all thoughts and to find a thought by id

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// ************************ USER ROUTES ************************
 // Find all users
 app.get("/all-users", async (req, res) => {
   try {
@@ -29,6 +29,28 @@ app.get("/user/:id", async (req, res) => {
     console.error(err);
   }
 });
+
+// ************************ THOUGHT ROUTES ************************
+// Find all thoughts
+app.get("/all-thoughts", async (req, res) => {
+    try {
+      const result = await Thought.find({});
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json({ message: "something went wrong" });
+    }
+  });
+
+// Find thought by id
+app.get("/thought/:id", async (req, res) => {
+    try {
+      const thoughtId = req.params.id;
+      const result = await Thought.findOne({ _id: thoughtId });
+      res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
 // Start the server once the database connection is open
 db.once("open", () => {
